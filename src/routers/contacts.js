9e2c32fee8809dc79e2c798c92b207bfb9f36ccd
MykolaLukianovych <lukjanovych@gmail.com>
2025-04-13 19:17:54 +0300

    finish

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getContactByIDController, getContactsController } from "../controllers/contacts.js";
+import { addContactController, deleteContactController, getContactByIDController, getContactsController, patchContactController } from "../controllers/contacts.js";
 import { controllerWrapper } from "../utils/controllerWrapper.js";
 
 
@@ -8,5 +8,11 @@ const contactsRouter = Router();
 contactsRouter.get("/", controllerWrapper(getContactsController));
 
 contactsRouter.get("/:contactId", controllerWrapper(getContactByIDController));
+
+contactsRouter.post("/", controllerWrapper(addContactController));
+
+contactsRouter.patch("/:contactId", controllerWrapper(patchContactController));
+
+contactsRouter.delete("/:contactId", controllerWrapper(deleteContactController));
 
 export default contactsRouter;
--- src/services/Contacts.js
+++ src/services/Contacts.js
@@ -3,4 +3,21 @@ import contactModel from "../db/models/Contact.js";
 
 export const getContacts = () => contactModel.find();
 
-export const getContactByID = contactId => contactModel.findOne({_id: contactId});
+export const getContactByID = contactId => contactModel.findOne({ _id: contactId });
+
+export const addContact = payload => contactModel.create(payload);
+
+export const updateContact = async (_id, payload) => {
+    const updatedContact = await contactModel.findOneAndUpdate({_id}, payload, {
+        new: true,
+        includeResultMetadata: true
+    });
+    
+    if (!updatedContact || !updatedContact.value) return null;
+
+    return {
+        data: updatedContact
+    };
+};
+
+export const deleteContact = contactId => contactModel.findOneAndDelete({ _id: contactId });
